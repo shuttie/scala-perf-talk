5ce395ea6946ef971bd9b64b5e01339b901f93c7
Roman Grebennikov <grebennikov.roman@gmail.com>
2015-03-09 16:56:46 +0300

    type-match: make selector class a benchmark parameter

--- src/main/scala/ru/jugvrn/ScalaTypeMatch.scala
+++ src/main/scala/ru/jugvrn/ScalaTypeMatch.scala
@@ -1,29 +1,53 @@
 package ru.jugvrn
 
-import org.openjdk.jmh.annotations.{Benchmark, Setup, State, Scope}
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+
+import scala.util.Random
 
 /**
  * Created by shutty on 12/25/14.
  */
 @State (Scope.Benchmark)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 class ScalaTypeMatch {
-  sealed trait I
-  class Foo extends I
-  class Bar extends I
-  class Baz extends I
 
-  var value:I = _
+  trait Base
+  class Foo extends Base
+  class Bar extends Base
+  class Baz extends Base
 
-  @Setup
+  @Param(Array("foo", "bar", "baz"))
+  var classType:String = _
+  var someClass:Base = _
+
+  @Setup(Level.Iteration)
   def prepare() = {
-    value = new Baz()
+    someClass = classType match {
+      case "foo" => new Foo()
+      case "bar" => new Bar()
+      case "baz" => new Baz()
+    }
   }
 
   @Benchmark
-  def pattern = value match {
+  def measurePatternMatch = someClass match {
     case _:Foo => 1
     case _:Bar => 2
     case _:Baz => 3
     case _ => 4
   }
+
+  @Benchmark
+  def measureIf = if (someClass.isInstanceOf[Foo])
+    1
+  else if (someClass.isInstanceOf[Bar])
+    2
+  else if (someClass.isInstanceOf[Baz])
+    3
+  else
+    4
+
 }
