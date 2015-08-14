3ddf66dfc30cc9f7a5051d88528d9a21b3ca9913
Roman Grebennikov <grebennikov.roman@gmail.com>
2015-08-14 08:53:42 +0300

    demo: hardcode timeunit

--- src/main/scala/ru/jugvrn/Demo.scala
+++ src/main/scala/ru/jugvrn/Demo.scala
@@ -1,7 +1,10 @@
 package ru.jugvrn
 
 import org.openjdk.jmh.annotations._
+import java.util.concurrent.TimeUnit
 
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 class Demo {
   @Benchmark
   def helloWorld = 42
